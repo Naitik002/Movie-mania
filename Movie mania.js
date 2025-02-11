b77f33d83d6029cf201c24486eb3aea3fa98b4cd
Naitik Raghuwanshi <raghuwanshinaitik02@gmail.com>
2025-02-11 18:24:46 +0530

    Initial commit

---
+++ Movie mania.js
@@ -0,0 +1,49 @@
+let btn = document.querySelector("button");
+let movediv = document.querySelector("div");
+
+const url = "http://www.omdbapi.com/?apikey=dc36c99f&s="
+
+
+let getMovies = async (movie) => {
+    let res = await fetch(url + movie)
+    console.log(res);
+    let data = await res.json()
+    console.log(data);
+    return data;
+}
+
+
+btn.addEventListener("click", async () => {
+    let movie = document.querySelector("input").value;
+
+    try {
+        let data = await getMovies(movie);
+        movediv.innerHTML = "";
+
+        if (!data.Search) {
+            movediv.innerHTML = "<p >No results found. Please try a different movie name.</p>";
+            alert
+            return;
+        }
+
+        data.Search.forEach(movie => {
+
+            let card = document.createElement("div");
+            card.classList.add("cardcss")
+
+            card.innerHTML = `
+            <img src="${movie.Poster}" alt="${movie.Title}">
+            <h3>${movie.Title}</h3>
+            <p>${movie.Year}</p>
+        `;
+
+                movediv.appendChild(card);
+
+        });
+        
+    } catch (error) {
+        movediv.innerHTML = "<p>Something went wrong. Please try again later.</p>";
+    }
+});
+
+
